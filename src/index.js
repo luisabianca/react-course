4a0ccdb6600309b84d04efbd970cb6d31b37e943
luisabianca <luisabianca93@gmail.com>
2022-08-02 23:19:54 -0300

    props

--- src/index.js
+++ src/index.js
@@ -3,28 +3,50 @@ import  ReactDOM  from "react-dom";
 //CSS
 import './index.css'
 
+const firstBook = {
+ title: 'O Peso do Pássaro Morto',
+ author: 'Aline Bei',
+ img: 'https://images-na.ssl-images-amazon.com/images/I/71cxQSe2m0L.jpg'
+}
+
+const secondBook = {
+ title: 'Flores para Algernon',
+ author: 'Daniel Keyes',
+ img: 'https://images-na.ssl-images-amazon.com/images/I/41tpztfvPML._SX330_BO1,204,203,200_.jpg'
+}
+
+const thirdBook = {
+ title: 'O incolor Tsukuru Tazaki e seus anos de peregrinação',
+ author: 'Haruki Murakami',
+ img: 'https://images-na.ssl-images-amazon.com/images/I/41TF1ikiiFL._SX318_BO1,204,203,200_.jpg'
+}
+
+const fourthBook = {
+ title: 'É assim que acaba',
+ author: 'Colleen Hoover',
+ img: 'https://images-na.ssl-images-amazon.com/images/I/91r5G8RxqfL.jpg'
+}
+
 function BookList() {
   return (
-    <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-    </section>
+    <div className="container">
+      <h1 className="title__booklist">Minha Estante</h1>
+      <section className="booklist">
+        <Book img={firstBook.img} author={firstBook.author} title={firstBook.title} />
+        <Book img={secondBook.img} author={secondBook.author} title={secondBook.title} />
+        <Book img={thirdBook.img} author={thirdBook.author} title={thirdBook.title} />
+        <Book img={fourthBook.img} author={fourthBook.author} title={fourthBook.title} />
+      </section>
+    </div>
   )
 }
 
-const Book = () => {
-  const title = 'O Peso do Pássaro Morto'
-  const author = 'Aline Bei'
+const Book = (props) => {
   return (
     <article className="book">
-      <img src="https://images-na.ssl-images-amazon.com/images/I/71cxQSe2m0L.jpg" alt="book" width={200}/>
-      <h1>{title}</h1>
-      <h4>{author.toUpperCase()}</h4>
+      <img src={props.img} alt="book" width={200}/>
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   )
 }
