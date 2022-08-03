e72f6565fdf09c7fc7c8b3a2eb948a32817f468b
luisabianca <luisabianca93@gmail.com>
2022-08-02 21:41:54 -0300

    init project books list for study

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import  ReactDOM  from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//CSS
+import './index.css'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  )
+}
+
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  )
+}
+
+const Image = () => {
+  return (
+    <img src="https://images-na.ssl-images-amazon.com/images/I/71cxQSe2m0L.jpg" alt="book" width={200}/>
+  )
+}
+
+const Title = () => <h1>O Peso do Pássaro Morto</h1>;
+const Author = () => <h4>Aline Bei</h4>;
+
+ReactDOM.render(<BookList/>, document.getElementById('root'));
